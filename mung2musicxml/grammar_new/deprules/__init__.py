4c66eb45cabae76afed6d2e90c54c33b15e97976
v-dvorak <dvorak@ufal.mff.cuni.cz>
2025-08-24 10:46:22 +0200

    Fix deprules loading logging

--- mung2musicxml/grammar_new/deprules/__init__.py
+++ mung2musicxml/grammar_new/deprules/__init__.py
@@ -7,9 +7,9 @@ def _load_grammar_part(data_file_name: str, name: str) -> str:
     local_dir = Path(__file__).parent
     data_file = local_dir / data_file_name
     with open(data_file, "r", encoding="utf8") as f:
-        return f.read()
+        output = f.read()
     logger.info(f"Loaded {name} from {data_file}")
-
+    return output
 
 PRECEDENCE_GRAMMAR_DEPRULES_TEXT: str = _load_grammar_part("precedence.deprules", "Precedence Deprules")
 SYNTAX_GRAMMAR_DEPRULES_TEXT: str = _load_grammar_part("syntax.deprules", "Syntax Deprules")
