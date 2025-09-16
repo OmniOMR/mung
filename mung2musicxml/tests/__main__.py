8967744ca5d9c6f2dde768b86b21c26cf3fc7980
v-dvorak <dvorak@ufal.mff.cuni.cz>
2025-09-16 16:44:52 +0200

    Make additional argument passing possible for test module

--- mung2musicxml/tests/__main__.py
+++ mung2musicxml/tests/__main__.py
@@ -1,8 +1,22 @@
+"""
+Equivalent of:
+
+>>> python -m unittest discover . -p test_*.py
+
+if called like this:
+
+>>> python -m mung2musicxml.tests
+
+Supports additional argument passing, example:
+
+>>> python -m mung2musicxml.tests -v
+"""
 from unittest import main
+import sys
 
 
 if __name__ == "__main__":
     main(
         module=None,
-        argv=["", "discover", "-p", "test_*.py"]
+        argv=["", "discover", "-p", "test_*.py"] + sys.argv[1:]
     )
