a535f5be892339419044cddae49bb2f7a38316b8
v-dvorak <dvorak@ufal.mff.cuni.cz>
2025-12-10 17:05:16 +0100

    Update duration inference to allow inference at measure level without preceding measure

--- mung2musicxml/inference/__init__.py
+++ mung2musicxml/inference/__init__.py
@@ -1,3 +1,4 @@
 from .pitch_inference import PitchInferenceEngineWrapper, PitchDataConstants
 from .duration_inference import OnsetInferenceEngineWrapper, OnsetsInferenceEngineWrapperStrategy
 from mung2midi.inference import Pitch, Octave, Alter, Step, OnsetsInferenceStrategy
+from mung.constants import OnsetDataConstants
