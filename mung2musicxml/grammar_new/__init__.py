b42d168049a07eb6ec35028624c1349a3936ad63
v-dvorak <dvorak@ufal.mff.cuni.cz>
2026-07-28 19:40:19 +0200

    Upload grammar impls from MuNG Studio

--- mung2musicxml/grammar_new/__init__.py
+++ mung2musicxml/grammar_new/__init__.py
@@ -1,2 +1,5 @@
 from .grammar import Grammar
 from .wrapper import NotationGraphGrammarWrapper
+from .impl.grammar_alphabet import GRAMMAR_ALPHABET
+from .impl.grammar_precedence import GRAMMAR_PRECEDENCE
+from .impl.grammar_syntax import GRAMMAR_SYNTAX
